9cc7d92cbf9dbb17ad6a67452b42cd66df138141
Taishi Kasuga <supercaracal@yahoo.com>
2025-05-26 09:14:54 +0900

    Fix 22

--- 0022_generate_parentheses.rs
+++ 0022_generate_parentheses.rs
@@ -9,6 +9,30 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn generate_parenthesis(n: i32) -> Vec<String> {
-    // TODO: impl
-    Vec::with_capacity(n as usize)
+    let mut ret = vec![];
+    let size = (n * 2) as usize;
+    let mut s = String::with_capacity(size);
+    backtrack(0, &mut s, size, &mut ret);
+    ret
+}
+
+fn backtrack(stack: usize, s: &mut String, size: usize, ret: &mut Vec<String>) {
+    if s.len() == size {
+        ret.push(s.clone());
+        return;
+    }
+    if (size - s.len()) > stack {
+        s.push('(');
+        backtrack(stack + 1, s, size, ret);
+        s.pop();
+        if stack > 0 {
+            s.push(')');
+            backtrack(stack - 1, s, size, ret);
+            s.pop();
+        }
+    } else {
+        s.push(')');
+        backtrack(stack - 1, s, size, ret);
+        s.pop();
+    }
 }
