85b23ad77fcc43c74aa16d2d459ac3ccafa409e7
Taishi Kasuga <supercaracal@yahoo.com>
2025-05-26 09:30:12 +0900

    Add 23

---
+++ 0023_merge_k_sorted_lists.rs
@@ -0,0 +1,66 @@
+#[derive(Clone)]
+struct ListNode {
+    val: i32,
+    next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    fn new(val: i32) -> Self {
+        ListNode { val, next: None }
+    }
+}
+
+impl std::fmt::Debug for ListNode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut list = f.debug_list();
+        let mut node = self;
+        loop {
+            list.entry(&node.val);
+            if node.next.is_none() {
+                break;
+            }
+            node = node.next.as_ref().unwrap();
+        }
+        list.finish()
+    }
+}
+
+fn parse_arg(arg: &str) -> Vec<i32> {
+    arg.split(',').map(|e| e.parse::<i32>().unwrap()).collect()
+}
+
+fn build_linked_list(nums: Vec<i32>) -> Option<Box<ListNode>> {
+    let mut root: Option<Box<ListNode>> = None;
+    let mut prev: Option<&mut ListNode> = None;
+    for n in nums {
+        let node = Box::new(ListNode::new(n));
+        match prev {
+            Some(p) => {
+                p.next = Some(node);
+                prev = Some(p.next.as_mut().unwrap());
+            }
+            None => {
+                root = Some(node);
+                prev = Some(root.as_mut().unwrap());
+            }
+        }
+    }
+    root
+}
+
+fn main() -> Result<(), &'static str> {
+    let args: Vec<String> = std::env::args().collect();
+    let mut lists = Vec::with_capacity(args.len() - 1);
+    for i in 1..args.len() {
+        let list = build_linked_list(parse_arg(&args[i]));
+        lists.push(list)
+    }
+    println!("{:?}", merge_k_lists(lists).unwrap());
+    Ok(())
+}
+
+fn merge_k_lists(lists: Vec<Option<Box<ListNode>>>) -> Option<Box<ListNode>> {
+    // TODO: impl
+    println!("{lists:?}");
+    Some(Box::new(ListNode::new(0)))
+}
