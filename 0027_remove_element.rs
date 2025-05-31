8d3fd68311825a85fbda99538c94a23c5f2bf337
Taishi Kasuga <supercaracal@yahoo.com>
2025-05-31 09:22:57 +0900

    Fix 27

--- 0027_remove_element.rs
+++ 0027_remove_element.rs
@@ -13,7 +13,23 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
-    // TODO: solve
-    println!("nums={nums:?}, val={val:?}");
-    0
+    let mut i = 0;
+    let mut j = nums.len() - 1;
+    while i < j {
+        if nums[i] == val && nums[j] != val {
+            nums[i] = nums[j];
+            nums[j] = val;
+        }
+        if nums[i] != val {
+            i += 1;
+        }
+        if nums[j] == val {
+            j -= 1;
+        }
+    }
+    println!("{nums:?}");
+    if i == 0 {
+        return 0;
+    }
+    (i + 1) as i32
 }
