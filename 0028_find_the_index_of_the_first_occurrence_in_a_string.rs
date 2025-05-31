8cfcdae3aed73e4a7759939aba801a77bc7a4cbe
Taishi Kasuga <supercaracal@yahoo.com>
2025-06-01 08:44:55 +0900

    Fix 28

--- 0028_find_the_index_of_the_first_occurrence_in_a_string.rs
+++ 0028_find_the_index_of_the_first_occurrence_in_a_string.rs
@@ -8,7 +8,20 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn str_str(haystack: String, needle: String) -> i32 {
-    // TODO: solve
-    println!("{haystack:?}, {needle:?}");
-    0
+    let haystack: Vec<char> = haystack.chars().collect();
+    let needle: Vec<char> = needle.chars().collect();
+    for i in 0..haystack.len() {
+        for j in 0..needle.len() {
+            if i + j >= haystack.len() {
+                return -1;
+            }
+            if haystack[i + j] != needle[j] {
+                break;
+            }
+            if j == needle.len() - 1 {
+                return i as i32;
+            }
+        }
+    }
+    -1
 }
