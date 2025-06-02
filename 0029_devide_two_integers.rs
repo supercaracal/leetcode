7158643e199aebee7ac1109bc573b3e5d7f2a9e7
Taishi Kasuga <supercaracal@yahoo.com>
2025-06-02 09:04:06 +0900

    Fix 29

--- 0029_devide_two_integers.rs
+++ 0029_devide_two_integers.rs
@@ -10,6 +10,30 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn divide(dividend: i32, divisor: i32) -> i32 {
-    // TODO: solve
-    dividend + divisor
+    if divisor == 1 {
+        return dividend;
+    }
+    if divisor == -1 {
+        if dividend == i32::MIN {
+            return i32::MAX;
+        } else {
+            return dividend * -1;
+        }
+    }
+    let mut q = 0;
+    let mut n = if dividend > 0 {
+        -1 * dividend
+    } else {
+        dividend
+    };
+    let m = if divisor > 0 { -1 * divisor } else { divisor };
+    while n <= m {
+        n -= m;
+        q += 1;
+    }
+    if (dividend < 0) == (divisor < 0) {
+        1 * q
+    } else {
+        -1 * q
+    }
 }
