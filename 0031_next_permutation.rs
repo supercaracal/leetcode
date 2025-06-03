9e89cbd9b612f172b1cb87d584b45857d7232f33
Taishi Kasuga <supercaracal@yahoo.com>
2025-06-03 10:43:45 +0900

    Add 31

---
+++ 0031_next_permutation.rs
@@ -0,0 +1,18 @@
+fn main() -> Result<(), &'static str> {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        return Err("usage: 1,2,3");
+    }
+    let mut nums: Vec<i32> = args[1]
+        .split(',')
+        .map(|e| e.parse::<i32>().unwrap())
+        .collect();
+    next_permutation(&mut nums);
+    println!("{:?}", nums);
+    Ok(())
+}
+
+fn next_permutation(nums: &mut Vec<i32>) {
+    // TODO: solve
+    println!("{nums:?}");
+}
