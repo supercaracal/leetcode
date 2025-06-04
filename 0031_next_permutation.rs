ca1780bd87d232a86019ef1710803b0bdc9ba645
Taishi Kasuga <supercaracal@yahoo.com>
2025-06-04 09:19:26 +0900

    Fix 31

--- 0031_next_permutation.rs
+++ 0031_next_permutation.rs
@@ -13,6 +13,30 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn next_permutation(nums: &mut Vec<i32>) {
-    // TODO: solve
-    println!("{nums:?}");
+    if nums.is_empty() || nums.len() == 1 {
+        return;
+    }
+    let mut i = nums.len() - 2;
+    while nums[i + 1] <= nums[i] {
+        i -= 1;
+    }
+    let mut j = nums.len() - 1;
+    while nums[j] <= nums[i] {
+        j -= 1;
+    }
+    swap(nums, i, j);
+    let mut l = i + 1;
+    let mut r = nums.len() - 1;
+    while l < r {
+        swap(nums, l, r);
+        l += 1;
+        r -= 1;
+    }
+}
+
+#[inline]
+fn swap(nums: &mut Vec<i32>, i: usize, j: usize) {
+    let tmp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = tmp;
 }
