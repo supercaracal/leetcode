d6862806a258be15a4297cb1fb9978e59b4575f7
Taishi Kasuga <supercaracal@yahoo.com>
2025-06-06 09:59:35 +0900

    Fix 33

--- 0033_search_in_rotated_sorted_array.rs
+++ 0033_search_in_rotated_sorted_array.rs
@@ -12,7 +12,28 @@ fn main() -> Result<(), &'static str> {
     Ok(())
 }
 
+// https://www.youtube.com/watch?v=U8XENwh8Oy8
 fn search(nums: Vec<i32>, target: i32) -> i32 {
-    println!("{nums:?}, {target:?}");
-    0
+    let mut l = 0;
+    let mut r = nums.len() - 1;
+    while l <= r {
+        let mid = (l + r) / 2;
+        if target == nums[mid] {
+            return mid as i32;
+        }
+        if nums[l] <= nums[mid] {
+            if target > nums[mid] || target < nums[l] {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        } else {
+            if target < nums[mid] || target > nums[r] {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+    }
+    -1
 }
