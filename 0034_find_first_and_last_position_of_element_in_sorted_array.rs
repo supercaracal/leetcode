ed3eae0d54701a2972378705ed174ec8b0031ff8
Taishi Kasuga <supercaracal@yahoo.com>
2025-06-07 09:49:15 +0900

    Fix 34

--- 0034_find_first_and_last_position_of_element_in_sorted_array.rs
+++ 0034_find_first_and_last_position_of_element_in_sorted_array.rs
@@ -14,10 +14,10 @@ fn main() -> Result<(), &'static str> {
 
 // https://www.youtube.com/watch?v=4sQL7R5ySUU
 fn search_range(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    let mut indices = Vec::with_capacity(2);
-    indices.push(bin_search(&nums, target, true));
-    indices.push(bin_search(&nums, target, false));
-    indices
+    vec![
+        bin_search(&nums, target, true),
+        bin_search(&nums, target, false),
+    ]
 }
 
 fn bin_search(nums: &[i32], target: i32, left_bias: bool) -> i32 {
@@ -27,30 +27,35 @@ fn bin_search(nums: &[i32], target: i32, left_bias: bool) -> i32 {
     let mut l = 0;
     let mut r = nums.len() - 1;
     let mut i = -1;
+    use std::cmp::Ordering;
     while l <= r {
         let m = (l + r) / 2;
-        if target < nums[m] {
-            if m == 0 {
-                break;
-            }
-            r = m - 1;
-        } else if target > nums[m] {
-            if m == nums.len() - 1 {
-                break;
-            }
-            l = m + 1;
-        } else {
-            i = m as i32;
-            if left_bias {
+        match nums[m].cmp(&target) {
+            Ordering::Greater => {
                 if m == 0 {
                     break;
                 }
                 r = m - 1;
-            } else {
+            }
+            Ordering::Less => {
                 if m == nums.len() - 1 {
                     break;
                 }
                 l = m + 1;
+            }
+            Ordering::Equal => {
+                i = m as i32;
+                if left_bias {
+                    if m == 0 {
+                        break;
+                    }
+                    r = m - 1;
+                } else {
+                    if m == nums.len() - 1 {
+                        break;
+                    }
+                    l = m + 1;
+                }
             }
         }
     }
