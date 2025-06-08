11d03ca4d91f6de862ba84bd99397c17709ddaba
Taishi Kasuga <supercaracal@yahoo.com>
2025-06-09 08:52:22 +0900

    Fix 36

--- 0036_valid_sudoku.rs
+++ 0036_valid_sudoku.rs
@@ -13,8 +13,49 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
-    for row in board.iter() {
-        println!("{row:?}");
+    for i in 0..9 {
+        let mut row = [false; 10];
+        let mut col = [false; 10];
+        for j in 0..9 {
+            if board[i][j] == '.' {
+                continue;
+            }
+            let n = board[i][j].to_digit(10).unwrap() as usize;
+            if row[n] {
+                return false;
+            }
+            row[n] = true;
+        }
+        for j in 0..9 {
+            if board[j][i] == '.' {
+                continue;
+            }
+            let n = board[j][i].to_digit(10).unwrap() as usize;
+            if col[n] {
+                return false;
+            }
+            col[n] = true;
+        }
     }
-    false
+    for i in 0..3 {
+        for j in 0..3 {
+            let mut blk = [false; 10];
+            for k in 0..3 {
+                for l in 0..3 {
+                    let x = i * 3 + k;
+                    let y = j * 3 + l;
+                    println!("{x:?},{y:?}");
+                    if board[y][x] == '.' {
+                        continue;
+                    }
+                    let n = board[y][x].to_digit(10).unwrap() as usize;
+                    if blk[n] {
+                        return false;
+                    }
+                    blk[n] = true;
+                }
+            }
+        }
+    }
+    true
 }
