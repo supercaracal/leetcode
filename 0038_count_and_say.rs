4d7121839e43ab8f8335cbaf7571177d0aef2ecb
Taishi Kasuga <supercaracal@yahoo.com>
2025-06-11 08:57:00 +0900

    Fix 38

--- 0038_count_and_say.rs
+++ 0038_count_and_say.rs
@@ -9,7 +9,29 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn count_and_say(n: i32) -> String {
-    // TODO: solve
-    println!("{n}");
-    "!".to_string()
+    if n == 0 {
+        return "".to_string();
+    }
+    if n == 1 {
+        return "1".to_string();
+    }
+    let mut encoded: Vec<(char, usize)> = Vec::new();
+    for c in count_and_say(n - 1).chars() {
+        if let Some(mut e) = encoded.pop() {
+            if c == e.0 {
+                e.1 += 1;
+                encoded.push(e);
+            } else {
+                encoded.push(e);
+                encoded.push((c, 1));
+            }
+        } else {
+            encoded.push((c, 1));
+        }
+    }
+    encoded.iter().fold("".to_string(), |mut acc, e| {
+        acc.push_str(e.1.to_string().as_str());
+        acc.push(e.0);
+        acc
+    })
 }
