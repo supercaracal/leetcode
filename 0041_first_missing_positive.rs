0240361239930480a54ecabb5e1ac7b8801c276d
Taishi Kasuga <supercaracal@yahoo.com>
2025-06-13 09:58:04 +0900

    Add 41

---
+++ 0041_first_missing_positive.rs
@@ -0,0 +1,18 @@
+fn main() -> Result<(), &'static str> {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        return Err("usage: 1,2,0");
+    }
+    let nums: Vec<i32> = args[1]
+        .split(',')
+        .map(|e| e.parse::<i32>().unwrap())
+        .collect();
+    println!("{:?}", first_missing_positive(nums));
+    Ok(())
+}
+
+fn first_missing_positive(nums: Vec<i32>) -> i32 {
+    // TODO: solve
+    println!("{nums:?}");
+    0
+}
