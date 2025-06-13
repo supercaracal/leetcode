38911a2b49b6b464c3e9dfa4badf6adb8f533651
Taishi Kasuga <supercaracal@yahoo.com>
2025-06-13 09:54:04 +0900

    Fix 40

--- 0040_combination_sum_ii.rs
+++ 0040_combination_sum_ii.rs
@@ -13,7 +13,35 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn combination_sum2(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
-    // TODO: solve
-    println!("{candidates:?}, {target:?}");
-    vec![vec![]]
+    let mut candidates = candidates;
+    candidates.sort();
+    let mut comb = Vec::new();
+    let mut combs = Vec::new();
+    backtrack(&candidates, target, 0, &mut comb, &mut combs);
+    combs
+}
+
+fn backtrack(
+    candidates: &[i32],
+    target: i32,
+    start: usize,
+    comb: &mut Vec<i32>,
+    combs: &mut Vec<Vec<i32>>,
+) {
+    if target == 0 {
+        combs.push(comb.clone());
+        return;
+    }
+    for i in start..candidates.len() {
+        if i > start && candidates[i] == candidates[i - 1] {
+            continue;
+        }
+        let n = candidates[i];
+        if n > target {
+            break;
+        }
+        comb.push(n);
+        backtrack(candidates, target - n, i + 1, comb, combs);
+        comb.pop();
+    }
 }
