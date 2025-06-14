6a37c5ab6bea0c14bae38fd0f7e74a370a6b723a
Taishi Kasuga <supercaracal@yahoo.com>
2025-06-14 12:26:33 +0900

    Fix 41

--- 0041_first_missing_positive.rs
+++ 0041_first_missing_positive.rs
@@ -12,7 +12,36 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn first_missing_positive(nums: Vec<i32>) -> i32 {
-    // TODO: solve
-    println!("{nums:?}");
-    0
+    // TODO: use cycle sort
+    let mut nums = nums;
+    let n = nums.len();
+    let mut contains1 = false;
+    for i in 0..n {
+        if nums[i] == 1 {
+            contains1 = true;
+        }
+        if nums[i] < 1 || nums[i] > (n as i32) {
+            nums[i] = 1;
+        }
+    }
+    if !contains1 {
+        return 1;
+    }
+    for i in 0..n {
+        let j = nums[i].abs() as usize;
+        if j == n {
+            nums[0] = -(nums[0].abs());
+        } else {
+            nums[j] = -(nums[j].abs());
+        }
+    }
+    for i in 1..n {
+        if nums[i] > 0 {
+            return i as i32;
+        }
+    }
+    if nums[0] > 0 {
+        return n as i32;
+    }
+    n as i32 + 1
 }
