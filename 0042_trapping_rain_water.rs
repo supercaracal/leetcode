fbc24372bd053c6cdc18bb736eec12a33c6ad62d
Taishi Kasuga <supercaracal@yahoo.com>
2025-06-15 09:59:51 +0900

    Fix 42

--- 0042_trapping_rain_water.rs
+++ 0042_trapping_rain_water.rs
@@ -12,7 +12,28 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn trap(height: Vec<i32>) -> i32 {
-    // TODO: solve
-    println!("{height:?}");
-    0
+    let len = height.len();
+    let mut l = 0;
+    let mut r = len - 1;
+    let mut max_l = 0;
+    let mut max_r = 0;
+    let mut area = 0;
+    while l < r {
+        max_l = max_l.max(height[l]);
+        max_r = max_r.max(height[r]);
+        if height[l] <= height[r] {
+            let n = max_l - height[l];
+            if n > 0 {
+                area += n;
+            }
+            l += 1;
+        } else {
+            let n = max_r - height[r];
+            if n > 0 {
+                area += n;
+            }
+            r -= 1;
+        }
+    }
+    area as i32
 }
