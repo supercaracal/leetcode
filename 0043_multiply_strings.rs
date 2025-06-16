2601048969282a39377c9b07a6b09eea5e7fb420
Taishi Kasuga <supercaracal@yahoo.com>
2025-06-16 09:12:28 +0900

    Fix 43

--- 0043_multiply_strings.rs
+++ 0043_multiply_strings.rs
@@ -8,6 +8,43 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn multiply(num1: String, num2: String) -> String {
-    // TODO: solve
-    num1 + &num2
+    if num1 == "0" || num2 == "0" {
+        return "0".to_string();
+    }
+    let mut sums = Vec::new();
+    let mut max = 0;
+    for (i, n1) in num1.chars().rev().enumerate() {
+        for (j, n2) in num2.chars().rev().enumerate() {
+            let p = n1.to_digit(10).unwrap();
+            let q = n2.to_digit(10).unwrap();
+            let mut sum = Vec::new();
+            for _ in 0..(i + j) {
+                sum.push(0);
+            }
+            let mut v = p * q;
+            while v > 0 {
+                sum.push(v % 10);
+                v /= 10;
+            }
+            max = max.max(sum.len());
+            sums.push(sum);
+        }
+    }
+    let mut ret = Vec::new();
+    let mut d = 0;
+    for i in 0..max {
+        for sum in sums.iter() {
+            if i < sum.len() {
+                d += sum[i];
+            }
+        }
+        ret.push(char::from_digit(d % 10, 10).unwrap());
+        d /= 10;
+    }
+    while d > 0 {
+        ret.push(char::from_digit(d % 10, 10).unwrap());
+        d /= 10;
+    }
+    ret.reverse();
+    ret.into_iter().collect()
 }
