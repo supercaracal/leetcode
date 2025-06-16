2ea77873124a0a59bc33dab1aaefc8d973d68b42
Taishi Kasuga <supercaracal@yahoo.com>
2025-06-17 08:35:13 +0900

    Add 45

---
+++ 0045_jump_game.rs
@@ -0,0 +1,18 @@
+fn main() -> Result<(), &'static str> {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        return Err("usage: 2,3,1,1,4");
+    }
+    let nums: Vec<i32> = args[1]
+        .split(',')
+        .map(|e| e.parse::<i32>().unwrap())
+        .collect();
+    println!("{:?}", jump(nums));
+    Ok(())
+}
+
+fn jump(nums: Vec<i32>) -> i32 {
+    // TODO: solve
+    println!("{nums:?}");
+    0
+}
