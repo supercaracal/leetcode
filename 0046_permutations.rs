e2e7e5a1abe8ace047b1734eca92f914130c6bc3
Taishi Kasuga <supercaracal@yahoo.com>
2025-06-19 09:53:50 +0900

    Fix 46

--- 0046_permutations.rs
+++ 0046_permutations.rs
@@ -12,7 +12,22 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
-    // TODO: solve
-    println!("{nums:?}");
-    vec![vec![]]
+    solve(nums.as_ref())
+}
+
+// https://www.youtube.com/watch?v=FZe0UqISmUw
+fn solve(nums: &[i32]) -> Vec<Vec<i32>> {
+    if nums.len() == 0 {
+        return vec![vec![]];
+    }
+    let perms = solve(&nums[1..]);
+    let mut res = vec![];
+    for p in perms {
+        for i in 0..=p.len() {
+            let mut c = p.clone();
+            c.insert(i, nums[0]);
+            res.push(c);
+        }
+    }
+    res
 }
