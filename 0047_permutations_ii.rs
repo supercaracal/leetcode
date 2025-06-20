ea74ed3c65e5669be746e667dd39d5d12225bf7d
Taishi Kasuga <supercaracal@yahoo.com>
2025-06-20 09:48:21 +0900

    Fix 47

--- 0047_permutations_ii.rs
+++ 0047_permutations_ii.rs
@@ -12,7 +12,44 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn permute_unique(nums: Vec<i32>) -> Vec<Vec<i32>> {
-    // TODO: solve
-    println!("{nums:?}");
-    vec![vec![]]
+    let mut p = Vec::with_capacity(nums.len());
+    let mut ps = Vec::with_capacity((1..=nums.len()).fold(1, |a, e| a * e));
+    let mut uniq = HashSet::new();
+    let mut counter = HashMap::new();
+    for n in nums.iter() {
+        let c = counter.get(n).map_or(0, |v| *v);
+        counter.insert(*n, c + 1);
+        uniq.insert(*n);
+    }
+    backtrack(nums.as_ref(), &mut p, &mut ps, &mut counter, &uniq);
+    ps
+}
+
+use std::collections::{HashMap, HashSet};
+
+fn backtrack(
+    nums: &[i32],
+    p: &mut Vec<i32>,
+    ps: &mut Vec<Vec<i32>>,
+    counter: &mut HashMap<i32, i32>,
+    uniq: &HashSet<i32>,
+) {
+    if p.len() == nums.len() {
+        ps.push(p.clone());
+        return;
+    }
+    for n in uniq.iter() {
+        let mut cnt = 0;
+        if let Some(c) = counter.get(n) {
+            if *c == 0 {
+                continue;
+            }
+            cnt = *c;
+        }
+        p.push(*n);
+        counter.insert(*n, cnt - 1);
+        backtrack(nums, p, ps, counter, uniq);
+        counter.insert(*n, cnt);
+        p.pop();
+    }
 }
