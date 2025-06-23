7ea55b89f95d4d14642fdd944c1e103137f431a7
Taishi Kasuga <supercaracal@yahoo.com>
2025-06-23 09:31:00 +0900

    Fix 50

--- 0050_pow_x_n.rs
+++ 0050_pow_x_n.rs
@@ -10,7 +10,24 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn my_pow(x: f64, n: i32) -> f64 {
-    // TODO: solve
-    println!("{x}^{n}");
-    0.0
+    if x == 0.0 {
+        return 0.0;
+    }
+    if n == 0 {
+        return 1.0;
+    }
+    let m = n.checked_abs().map_or(i32::MAX, |v| v);
+    let mut ret = my_pow(x, m / 2);
+    ret *= ret;
+    if m % 2 == 1 {
+        ret *= x;
+    }
+    if n.checked_abs().map_or(1, |_| 0) == 1 {
+        ret *= x;
+    }
+    if n.is_positive() {
+        ret
+    } else {
+        1.0 / ret
+    }
 }
