2879cc2dd20abb719e02edad87a9c0ba9bf62456
Taishi Kasuga <supercaracal@yahoo.com>
2025-06-24 10:30:41 +0900

    Fix 51

--- 0051_n_queens.rs
+++ 0051_n_queens.rs
@@ -4,12 +4,122 @@ fn main() -> Result<(), &'static str> {
         return Err("usage: 4");
     }
     let n = args[1].parse::<i32>().unwrap();
-    println!("{:?}", solve_n_queens(n));
+    for row in solve_n_queens(n) {
+        println!("{:?}", row);
+    }
     Ok(())
 }
 
 fn solve_n_queens(n: i32) -> Vec<Vec<String>> {
-    // TODO: solve
-    println!("{n}");
-    vec![vec![]]
+    let mut ret = Vec::new();
+    for i in 0..n {
+        let mut board = vec![vec!['.'; n as usize]; n as usize];
+        board[0][i as usize] = 'Q';
+        if !backtrack(&mut board) {
+            continue;
+        }
+        let ans = board
+            .iter()
+            .map(|row| {
+                let mut s = String::with_capacity(row.len());
+                for c in row {
+                    s.push(*c);
+                }
+                s
+            })
+            .collect();
+        ret.push(ans);
+    }
+    ret
+}
+
+fn backtrack(board: &mut Vec<Vec<char>>) -> bool {
+    if board.len() < 2 {
+        return true;
+    }
+    for r in 1..board.len() {
+        for c in 0..board.len() {
+            if board[r][c] == 'Q' {
+                continue;
+            }
+            if !is_safe(r, c, board) {
+                continue;
+            }
+            board[r][c] = 'Q';
+            if backtrack(board) {
+                return true;
+            }
+            board[r][c] = '.';
+        }
+        if board[r].iter().all(|c| *c == '.') {
+            return false;
+        }
+    }
+    true
+}
+
+fn is_safe(r: usize, c: usize, board: &Vec<Vec<char>>) -> bool {
+    if board[r][c] == 'Q' {
+        return false;
+    }
+    for row in board.iter() {
+        if row[c] == 'Q' {
+            return false;
+        }
+    }
+    for c in &board[r] {
+        if *c == 'Q' {
+            return false;
+        }
+    }
+    let size = board.len();
+    let mut i = r;
+    let mut j = c;
+    loop {
+        if board[i][j] == 'Q' {
+            return false;
+        }
+        if i == size - 1 || j == size - 1 {
+            break;
+        }
+        i += 1;
+        j += 1;
+    }
+    i = r;
+    j = c;
+    loop {
+        if board[i][j] == 'Q' {
+            return false;
+        }
+        if i == 0 || j == 0 {
+            break;
+        }
+        i -= 1;
+        j -= 1;
+    }
+    i = r;
+    j = c;
+    loop {
+        if board[i][j] == 'Q' {
+            return false;
+        }
+        if i == size - 1 || j == 0 {
+            break;
+        }
+        i += 1;
+        j -= 1;
+    }
+    i = r;
+    j = c;
+    loop {
+        if board[i][j] == 'Q' {
+            return false;
+        }
+        if i == 0 || j == size - 1 {
+            break;
+        }
+        i -= 1;
+        j += 1;
+    }
+    true
 }
