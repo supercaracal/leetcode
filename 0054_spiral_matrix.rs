c2c9c514ecf1d28494aff36a2b10db455d26f1ab
Taishi Kasuga <supercaracal@yahoo.com>
2025-06-25 08:51:25 +0900

    Add 54

---
+++ 0054_spiral_matrix.rs
@@ -0,0 +1,30 @@
+fn main() -> Result<(), &'static str> {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 3 {
+        return Err("usage: 1,2,3,4,5,6,7,8,9 3");
+    }
+    let size = args[2].parse::<usize>().unwrap();
+    let mut matrix = Vec::with_capacity(size);
+    for nums in args[1]
+        .split(',')
+        .map(|e| e.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>()
+        .chunks(size)
+    {
+        let mut row = Vec::with_capacity(size);
+        for n in nums {
+            row.push(*n);
+        }
+        matrix.push(row);
+    }
+    println!("{:?}", spiral_order(matrix));
+    Ok(())
+}
+
+fn spiral_order(matrix: Vec<Vec<i32>>) -> Vec<i32> {
+    // TODO: solve
+    for row in matrix {
+        println!("{row:?}");
+    }
+    vec![]
+}
