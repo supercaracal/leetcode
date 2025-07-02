b207621845f3ed14897df139e21ab50228460183
Taishi Kasuga <supercaracal@yahoo.com>
2025-07-02 09:01:23 +0900

    Fix 60

--- 0060_permutation_sequence.rs
+++ 0060_permutation_sequence.rs
@@ -10,6 +10,28 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn get_permutation(n: i32, k: i32) -> String {
-    println!("n={n}, k={k}");
-    "TODO: solve".to_string()
+    let mut nums = (1..=n).collect();
+    let mut perms = Vec::with_capacity((1..=n).fold(1, |a, e| a * e) as usize);
+    let mut perm = Vec::with_capacity(n as usize);
+    backtrack(n, &mut nums, &mut perm, &mut perms);
+    let mut s = String::with_capacity(n as usize);
+    for c in perms[(k - 1) as usize]
+        .iter()
+        .map(|e| char::from_digit(*e as u32, 10).unwrap())
+    {
+        s.push(c);
+    }
+    s
+}
+
+fn backtrack(n: i32, nums: &mut Vec<i32>, perm: &mut Vec<i32>, perms: &mut Vec<Vec<i32>>) {
+    if perm.len() == (n as usize) {
+        perms.push(perm.clone());
+        return;
+    }
+    for i in 0..nums.len() {
+        perm.push(nums.remove(i));
+        backtrack(n, nums, perm, perms);
+        nums.insert(i, perm.pop().unwrap());
+    }
 }
