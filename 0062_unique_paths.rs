dc30f7383ce9cf7402858d8b6d50e3ff6c27b1e5
Taishi Kasuga <supercaracal@yahoo.com>
2025-07-04 10:22:54 +0900

    Fix 62

--- 0062_unique_paths.rs
+++ 0062_unique_paths.rs
@@ -10,6 +10,20 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn unique_paths(m: i32, n: i32) -> i32 {
-    // TODO: solve
-    m + n
+    // 1  1  1  1  1  1  1
+    // 1  2  3  4  5  6  7
+    // 1  3  6 10 15 21 28
+    let m = m as usize;
+    let n = n as usize;
+    let mut matrix = vec![vec![0; n]; m];
+    for i in 0..m {
+        for j in 0..n {
+            if i == 0 || j == 0 {
+                matrix[i][j] = 1;
+                continue;
+            }
+            matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1];
+        }
+    }
+    matrix[m - 1][n - 1]
 }
