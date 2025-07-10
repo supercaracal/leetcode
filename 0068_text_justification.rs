ead32059d46dc7c88ce14c109615260a82965003
Taishi Kasuga <supercaracal@yahoo.com>
2025-07-10 10:32:33 +0900

    Fix 68

--- 0068_text_justification.rs
+++ 0068_text_justification.rs
@@ -19,8 +19,8 @@ fn full_justify(words: Vec<String>, max_width: i32) -> Vec<String> {
     let mut count = 0;
     for i in 0..words.len() {
         let min_sp_cnt = if count == 0 { 0 } else { count - 1 };
-        if max_width < width + min_sp_cnt + words[i].len() {
-            let line = justify(max_width, width, count, start..i, &words);
+        if max_width <= width + min_sp_cnt + words[i].len() && start < i {
+            let line = justify(max_width, width, count, start..i, &words, false);
             ret.push(line);
             start = i;
             width = 0;
@@ -28,10 +28,10 @@ fn full_justify(words: Vec<String>, max_width: i32) -> Vec<String> {
         }
         width += words[i].len();
         count += 1;
-    }
-    if width > 0 {
-        let line = justify(max_width, width, count, start..words.len(), &words);
-        ret.push(line);
+        if i == words.len() - 1 {
+            let line = justify(max_width, width, count, start..words.len(), &words, true);
+            ret.push(line);
+        }
     }
     ret
 }
@@ -42,6 +42,7 @@ fn justify(
     count: usize,
     indices: std::ops::Range<usize>,
     words: &[String],
+    is_last: bool,
 ) -> String {
     let mut line = String::with_capacity(max_width);
     let mut total_sp_cnt = max_width - width;
@@ -49,6 +50,13 @@ fn justify(
     let mut sp_cnt_r = total_sp_cnt % (count.max(2) - 1);
     for i in indices {
         line.push_str(words[i].as_str());
+        if max_width == line.len() {
+            continue;
+        }
+        if is_last {
+            line.push(' ');
+            continue;
+        }
         if total_sp_cnt < 1 {
             continue;
         }
@@ -60,7 +68,14 @@ fn justify(
             sp_cnt_r -= 1;
             total_sp_cnt -= sp_cnt;
         }
-        total_sp_cnt -= sp_cnt;
+        if total_sp_cnt >= sp_cnt {
+            total_sp_cnt -= sp_cnt;
+        }
+    }
+    if max_width > line.len() {
+        for _ in 0..(max_width - line.len()) {
+            line.push(' ');
+        }
     }
     line
 }
