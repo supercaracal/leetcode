bbf4cbd57fa65d1442c93288532c11806982077c
Taishi Kasuga <supercaracal@yahoo.com>
2025-07-12 09:08:50 +0900

    Fix 70

--- 0070_climbing_stairs.rs
+++ 0070_climbing_stairs.rs
@@ -9,23 +9,27 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn climb_stairs(n: i32) -> i32 {
-    let mut step = Vec::new();
-    let mut steps = Vec::new();
-    backtrack(n, &mut step, &mut steps);
-    steps.len() as i32
+    let mut cache = HashMap::new();
+    // TODO: optimize, N(k) = N(k - 1) + N(k - 2)
+    backtrack(n, &mut cache)
 }
 
-fn backtrack(n: i32, step: &mut Vec<i32>, steps: &mut Vec<Vec<i32>>) {
+use std::collections::HashMap;
+
+fn backtrack(n: i32, cache: &mut HashMap<i32, i32>) -> i32 {
+    if let Some(cnt) = cache.get(&n) {
+        return *cnt;
+    }
     if n < 0 {
-        return;
+        return 0;
     }
     if n == 0 {
-        steps.push(step.clone());
-        return;
+        return 1;
     }
+    let mut cnt = 0;
     for i in 1..=2 {
-        step.push(i);
-        backtrack(n - i, step, steps);
-        step.pop();
+        cnt += backtrack(n - i, cache);
     }
+    cache.insert(n, cnt);
+    cnt
 }
