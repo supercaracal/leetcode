04a8de45bfe025d0b30f115b780369811a316b3a
Taishi Kasuga <supercaracal@yahoo.com>
2025-07-19 08:56:44 +0900

    Fix 77

--- 0077_combinations.rs
+++ 0077_combinations.rs
@@ -10,7 +10,29 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn combine(n: i32, k: i32) -> Vec<Vec<i32>> {
-    // TODO: solve
-    println!("n={n}, k={k}");
-    vec![]
+    // TODO: optimize
+    let k = k as usize;
+    let nums: Vec<i32> = (1..=n).collect();
+    let mut comb = Vec::with_capacity(k);
+    let mut combs = Vec::new();
+    combination(k, 0, &nums, &mut comb, &mut combs);
+    combs
+}
+
+fn combination(
+    k: usize,
+    i: usize,
+    nums: &Vec<i32>,
+    comb: &mut Vec<i32>,
+    combs: &mut Vec<Vec<i32>>,
+) {
+    if comb.len() == k {
+        combs.push(comb.clone());
+        return;
+    }
+    for j in i..nums.len() {
+        comb.push(nums[j]);
+        combination(k, j + 1, nums, comb, combs);
+        comb.pop();
+    }
 }
