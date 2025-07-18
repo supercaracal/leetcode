c9e18a3447c32df8ae7bc6a0019f0337784dbb87
Taishi Kasuga <supercaracal@yahoo.com>
2025-07-18 12:24:25 +0900

    Fix 76

--- 0076_minimum_window_substring.rs
+++ 0076_minimum_window_substring.rs
@@ -8,62 +8,43 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn min_window(s: String, t: String) -> String {
+    // TODO: fix
     use std::collections::HashMap;
-    let mut map: HashMap<char, Vec<usize>> = HashMap::new();
-    for (i, c) in s.chars().enumerate() {
-        let indices = map.entry(c).or_default();
-        indices.push(i);
-    }
-    let mut targets = Vec::with_capacity(t.len());
-    let mut seen: HashMap<char, usize> = HashMap::new();
+    let mut need: HashMap<char, usize> = HashMap::new();
     for c in t.chars() {
-        match map.get(&c) {
-            Some(indices) => targets.push(indices),
-            None => return "".to_string(),
-        }
-        seen.entry(c).and_modify(|e| *e += 1).or_insert(1);
+        need.entry(c).and_modify(|e| *e += 1).or_insert(1);
     }
-    for (k, v) in seen.iter() {
-        if let Some(indices) = map.get(k) {
-            if *v > indices.len() {
-                return "".to_string();
+    let need_cnt = need.len();
+    let mut have: HashMap<char, usize> = HashMap::new();
+    let mut have_cnt = 0usize;
+    let mut l = 0;
+    let mut min_l = 0;
+    let mut min_r = s.len() - 1;
+    for (r, c) in s.chars().enumerate() {
+        let hc = have.entry(c).and_modify(|e| *e += 1).or_insert(1);
+        let nc = need.get(&c).map_or(0, |e| *e);
+        if *hc == nc {
+            have_cnt += 1;
+        }
+        while have_cnt == need_cnt {
+            if min_r - min_l > r - l {
+                min_l = l;
+                min_r = r;
             }
+            let hc2 = have
+                .entry(c)
+                .and_modify(|e| *e = e.saturating_sub(1))
+                .or_insert(0);
+            if *hc2 < nc {
+                have_cnt = have_cnt.saturating_sub(1);
+            }
+            l += 1;
         }
     }
-    let mut perm = Vec::with_capacity(t.len());
-    let mut perms = Vec::new();
-    permutation(0, &targets, &mut perm, &mut perms);
-    let idx = perms
-        .iter()
-        .min_by(|x, y| {
-            let x_diff = x.iter().max().unwrap() - x.iter().min().unwrap();
-            let y_diff = y.iter().max().unwrap() - y.iter().min().unwrap();
-            x_diff.cmp(&y_diff)
-        })
-        .unwrap();
-    let s_chars: Vec<char> = s.chars().collect();
-    let mut ret = "".to_string();
-    let r = idx.iter().min().unwrap().clone() as usize;
-    let l = idx.iter().max().unwrap().clone() as usize;
-    for i in r..=l {
-        ret.push(s_chars[i]);
+    let mut ret = String::with_capacity(min_r - min_l + 1);
+    let s_ch: Vec<char> = s.chars().collect();
+    for i in min_r..=min_l {
+        ret.push(s_ch[i]);
     }
     ret
-}
-
-fn permutation(
-    i: usize,
-    targets: &Vec<&Vec<usize>>,
-    perm: &mut Vec<usize>,
-    perms: &mut Vec<Vec<usize>>,
-) {
-    if perm.len() == targets.len() {
-        perms.push(perm.clone());
-        return;
-    }
-    for j in 0..targets[i].len() {
-        perm.push(targets[i][j]);
-        permutation(i + 1, targets, perm, perms);
-        perm.pop();
-    }
 }
