8579c758e2c5abcc983d0ac4e59753e994f45a63
Taishi Kasuga <supercaracal@yahoo.com>
2025-07-20 10:33:56 +0900

    Fix 78

--- 0078_subsets.rs
+++ 0078_subsets.rs
@@ -12,7 +12,20 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn subsets(nums: Vec<i32>) -> Vec<Vec<i32>> {
-    // TODO: solve
-    println!("{nums:?}");
-    vec![]
+    let mut subset = Vec::new();
+    let mut subsets = Vec::new();
+    backtrack(0, nums.as_ref(), &mut subset, &mut subsets);
+    subsets
+}
+
+fn backtrack(i: usize, nums: &[i32], subset: &mut Vec<i32>, subsets: &mut Vec<Vec<i32>>) {
+    subsets.push(subset.clone());
+    if subset.len() == nums.len() {
+        return;
+    }
+    for j in i..nums.len() {
+        subset.push(nums[j]);
+        backtrack(j + 1, nums, subset, subsets);
+        subset.pop();
+    }
 }
