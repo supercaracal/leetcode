08c9e3fd06776a3d40becdd08c2e2b0314433431
Taishi Kasuga <supercaracal@yahoo.com>
2025-07-26 14:50:23 +0900

    Fix 84

--- 0084_largest_rectangle_in_histogram.rs
+++ 0084_largest_rectangle_in_histogram.rs
@@ -12,7 +12,23 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn largest_rectangle_area(heights: Vec<i32>) -> i32 {
-    // TODO: solve
-    println!("{heights:?}");
-    0
+    // https://www.youtube.com/watch?v=zx5Sw9130L0
+    let mut max_area = 0;
+    let mut stack = Vec::with_capacity(heights.len());
+    for (i, h) in heights.iter().enumerate() {
+        let mut start = i;
+        while let Some((si, sh)) = stack.pop() {
+            if sh <= *h {
+                stack.push((si, sh));
+                break;
+            }
+            max_area = max_area.max(sh * ((i - si) as i32));
+            start = si;
+        }
+        stack.push((start, *h));
+    }
+    for (i, h) in stack.iter() {
+        max_area = max_area.max(h * ((heights.len() - i) as i32));
+    }
+    max_area
 }
