b923bbce2d6b84ddd8f1107c29d5f6897e21ce69
Taishi Kasuga <supercaracal@yahoo.com>
2025-07-29 09:41:39 +0900

    Fix 87

--- 0087_scramble_string.rs
+++ 0087_scramble_string.rs
@@ -8,6 +8,32 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn is_scramble(s1: String, s2: String) -> bool {
-    // TODO: solve
-    s1 == s2
+    let mut cache = HashMap::new();
+    dp(s1.as_str(), s2.as_str(), &mut cache)
+}
+
+use std::collections::HashMap;
+
+// https://www.youtube.com/watch?v=MDmZm_aVDF8
+fn dp<'a>(s1: &'a str, s2: &'a str, cache: &mut HashMap<(&'a str, &'a str), bool>) -> bool {
+    let n = s1.len();
+    if n == 1 {
+        return s1 == s2;
+    }
+    if s1 == s2 {
+        return true;
+    }
+    if let Some(ans) = cache.get(&(s1, s2)) {
+        return *ans;
+    }
+    for i in 1..n {
+        if (dp(&s1[0..i], &s2[0..i], cache) && dp(&s1[i..], &s2[i..], cache))
+            || (dp(&s1[..i], &s2[(n - i)..], cache) && dp(&s1[i..], &s2[..(n - i)], cache))
+        {
+            cache.insert((s1, s2), true);
+            return true;
+        }
+    }
+    cache.insert((s1, s2), false);
+    false
 }
