a60fdbb8b4db4cde887fb9f14d335a8bafc96121
Taishi Kasuga <supercaracal@yahoo.com>
2025-07-31 09:38:01 +0900

    Fix 89

--- 0089_gray_code.rs
+++ 0089_gray_code.rs
@@ -8,7 +8,13 @@ fn main() -> Result<(), &'static str> {
     Ok(())
 }
 
+// https://www.youtube.com/watch?v=CHr3V8JDa1w
 fn gray_code(n: i32) -> Vec<i32> {
-    // TODO: solve
-    vec![0; n as usize]
+    let size = 1 << n;
+    let mut ret = Vec::with_capacity(size);
+    for i in 0..size {
+        let v = i ^ (i >> 1); // TODO: figure out
+        ret.push(v as i32);
+    }
+    ret
 }
