6bb0c41f3c135b90b02406aa011b684953b3efdc
Taishi Kasuga <supercaracal@yahoo.com>
2025-08-02 09:45:52 +0900

    Fix 91

--- 0091_decode_ways.rs
+++ 0091_decode_ways.rs
@@ -8,37 +8,35 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn num_decodings(s: String) -> i32 {
-    let mut msg = Vec::new();
-    let mut msgs = Vec::new();
     let chars = s.chars().collect::<Vec<_>>();
-    backtrack(chars.as_slice(), 0, &mut msg, &mut msgs);
-    println!("{msgs:?}");
-    msgs.len() as i32
+    let mut cache = HashMap::new();
+    backtrack(chars.as_slice(), 0, &mut cache)
 }
 
-fn backtrack(chars: &[char], i: usize, msg: &mut Vec<u8>, msgs: &mut Vec<Vec<u8>>) {
+use std::collections::HashMap;
+
+fn backtrack(chars: &[char], i: usize, cache: &mut HashMap<usize, i32>) -> i32 {
+    if let Some(v) = cache.get(&i) {
+        return *v;
+    }
     if i == chars.len() {
-        msgs.push(msg.clone());
-        return;
+        return 1;
     }
     if chars[i] == '0' {
-        return;
+        return 0;
     }
     if i < chars.len() - 1 && chars[i] > '2' && chars[i + 1] == '0' {
-        return;
+        return 0;
     }
-    msg.push(chars[i].to_digit(10).map_or(0, |v| v) as u8);
-    backtrack(chars, i + 1, msg, msgs);
-    msg.pop();
+    let mut cnt = 0;
+    cnt += backtrack(chars, i + 1, cache);
     if i == chars.len() - 1 {
-        return;
+        return cnt;
     }
     if i < chars.len() - 1 && (chars[i] > '2' || (chars[i] == '2' && chars[i + 1] > '6')) {
-        return;
+        return cnt;
     }
-    let d10 = chars[i].to_digit(10).map_or(0, |v| v * 10);
-    let d1 = chars[i + 1].to_digit(10).map_or(0, |v| v);
-    msg.push((d10 + d1) as u8);
-    backtrack(chars, i + 2, msg, msgs);
-    msg.pop();
+    cnt += backtrack(chars, i + 2, cache);
+    cache.insert(i, cnt);
+    cnt
 }
