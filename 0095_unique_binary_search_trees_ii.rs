4e9bfe0e2446aaa13dd40338ddf8edf8718033cd
Taishi Kasuga <supercaracal@yahoo.com>
2025-08-05 08:09:35 +0900

    Add 95

---
+++ 0095_unique_binary_search_trees_ii.rs
@@ -0,0 +1,53 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(PartialEq, Eq)]
+struct TreeNode {
+    val: i32,
+    left: Option<Rc<RefCell<TreeNode>>>,
+    right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+fn dig(node: &TreeNode, list: &mut std::fmt::DebugList) {
+    if let Some(ref n) = node.left {
+        dig(&n.clone().borrow(), list);
+    }
+    list.entry(&node.val);
+    if let Some(ref n) = node.right {
+        dig(&n.clone().borrow(), list);
+    }
+}
+
+impl std::fmt::Debug for TreeNode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut list = f.debug_list();
+        dig(self, &mut list);
+        list.finish()
+    }
+}
+
+fn main() -> Result<(), &'static str> {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        return Err("usage: 3");
+    }
+    let n = args[1].parse::<i32>().unwrap();
+    println!("{:?}", generate_trees(n));
+    Ok(())
+}
+
+fn generate_trees(n: i32) -> Vec<Option<Rc<RefCell<TreeNode>>>> {
+    // TODO: solve
+    println!("{:?}", TreeNode::new(n));
+    vec![]
+}
