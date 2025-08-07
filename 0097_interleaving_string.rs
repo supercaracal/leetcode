a80f049f0cc9906bf64d56d028a34b0316e584d6
Taishi Kasuga <supercaracal@yahoo.com>
2025-08-08 08:57:12 +0900

    Fix 97

--- 0097_interleaving_string.rs
+++ 0097_interleaving_string.rs
@@ -11,7 +11,32 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn is_interleave(s1: String, s2: String, s3: String) -> bool {
-    // TODO: solve
-    println!("{s1:?} {s2:?} {s3:?}");
+    let mut cache = HashMap::new();
+    backtrack(s1.as_str(), s2.as_str(), s3.as_str(), &mut cache)
+}
+
+use std::collections::HashMap;
+
+fn backtrack(s1: &str, s2: &str, s3: &str, cache: &mut HashMap<(usize, usize), bool>) -> bool {
+    if s1.is_empty() && s2.is_empty() && s3.is_empty() {
+        return true;
+    }
+    if let Some(v) = cache.get(&(s1.len(), s2.len())) {
+        return *v;
+    }
+    if s1.len() > 0 && s3.len() > 0 && s1[0..1] == s3[0..1] {
+        let v = backtrack(s1[1..].as_ref(), s2, s3[1..].as_ref(), cache);
+        cache.insert((s1.len() - 1, s2.len()), v);
+        if v {
+            return true;
+        }
+    }
+    if s2.len() > 0 && s3.len() > 0 && s2[0..1] == s3[0..1] {
+        let v = backtrack(s1, s2[1..].as_ref(), s3[1..].as_ref(), cache);
+        cache.insert((s1.len(), s2.len() - 1), v);
+        if v {
+            return true;
+        }
+    }
     false
 }
