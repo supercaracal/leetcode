56326bba7981c5f4ae7df3c73d1791fc7f3e1e99
Taishi Kasuga <supercaracal@yahoo.com>
2025-08-08 08:57:44 +0900

    Fix 97

--- 0097_interleaving_string.rs
+++ 0097_interleaving_string.rs
@@ -24,14 +24,14 @@ fn backtrack(s1: &str, s2: &str, s3: &str, cache: &mut HashMap<(usize, usize), b
     if let Some(v) = cache.get(&(s1.len(), s2.len())) {
         return *v;
     }
-    if s1.len() > 0 && s3.len() > 0 && s1[0..1] == s3[0..1] {
+    if !s1.is_empty() && !s3.is_empty() && s1[0..1] == s3[0..1] {
         let v = backtrack(s1[1..].as_ref(), s2, s3[1..].as_ref(), cache);
         cache.insert((s1.len() - 1, s2.len()), v);
         if v {
             return true;
         }
     }
-    if s2.len() > 0 && s3.len() > 0 && s2[0..1] == s3[0..1] {
+    if !s2.is_empty() && !s3.is_empty() && s2[0..1] == s3[0..1] {
         let v = backtrack(s1, s2[1..].as_ref(), s3[1..].as_ref(), cache);
         cache.insert((s1.len(), s2.len() - 1), v);
         if v {
