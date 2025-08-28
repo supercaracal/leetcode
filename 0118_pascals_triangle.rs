e398c9bbaff4d811e1a8f6ec87668ab8e93d2b70
Taishi Kasuga <supercaracal@yahoo.com>
2025-08-28 09:09:25 +0900

    Fix 118

--- 0118_pascals_triangle.rs
+++ 0118_pascals_triangle.rs
@@ -4,12 +4,30 @@ fn main() -> Result<(), &'static str> {
         return Err("usage: 5");
     }
     let num_rows = args[1].parse::<i32>().unwrap();
-    println!("{:?}", generate(num_rows));
+    for row in generate(num_rows).iter() {
+        println!("{row:?}");
+    }
     Ok(())
 }
 
 fn generate(num_rows: i32) -> Vec<Vec<i32>> {
-    // TODO: solve
-    println!("{num_rows}");
-    vec![]
+    let num_rows = num_rows as usize;
+    let mut rows: Vec<Vec<i32>> = Vec::with_capacity(num_rows);
+    for i in 1..=num_rows {
+        let mut row = Vec::with_capacity(i);
+        if i == 1 {
+            row.push(1);
+        } else {
+            for j in 0..i {
+                if j == 0 || j == i - 1 {
+                    row.push(1);
+                } else {
+                    let v = rows[i - 2][j - 1] + rows[i - 2][j];
+                    row.push(v);
+                }
+            }
+        }
+        rows.push(row);
+    }
+    rows
 }
