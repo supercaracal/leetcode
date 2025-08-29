277782db525c5acd7df88d31d44e948b44d25f0f
Taishi Kasuga <supercaracal@yahoo.com>
2025-08-29 09:12:58 +0900

    Fix 119

--- 0119_pascals_triangle_ii.rs
+++ 0119_pascals_triangle_ii.rs
@@ -9,7 +9,22 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn get_row(row_index: i32) -> Vec<i32> {
-    // TODO: solve
-    println!("{row_index}");
-    vec![]
+    if row_index < 0 {
+        return Vec::with_capacity(0);
+    }
+    let row_index = row_index as usize;
+    let mut row = Vec::with_capacity(row_index + 1);
+    for i in 0..=row_index {
+        row.push(1);
+        let mut prev = 1;
+        for j in 0..=i {
+            if j == 0 || j == i {
+                continue;
+            }
+            let tmp = row[j];
+            row[j] = prev + row[j];
+            prev = tmp;
+        }
+    }
+    row
 }
