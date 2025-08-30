87be89fcf5857e6a483721522a66e245f4e53b72
Taishi Kasuga <supercaracal@yahoo.com>
2025-08-30 10:04:13 +0900

    Fix 120

--- 0120_triangle.rs
+++ 0120_triangle.rs
@@ -16,9 +16,30 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn minimum_total(triangle: Vec<Vec<i32>>) -> i32 {
-    // TODO: solve
-    for row in triangle.iter() {
-        println!("{row:?}");
+    use std::collections::HashMap;
+    fn backtrack(
+        triangle: &Vec<Vec<i32>>,
+        i: usize,
+        j: usize,
+        cache: &mut HashMap<(usize, usize), i32>,
+    ) -> i32 {
+        if let Some(v) = cache.get(&(i, j)) {
+            return *v;
+        }
+        let v = if i == triangle.len() || j == triangle[i].len() {
+            0
+        } else {
+            let l = triangle[i][j] + backtrack(triangle, i + 1, j, cache);
+            if j + 1 == triangle[i].len() {
+                l
+            } else {
+                let r = triangle[i][j + 1] + backtrack(triangle, i + 1, j + 1, cache);
+                l.min(r)
+            }
+        };
+        cache.insert((i, j), v);
+        v
     }
-    0
+    let mut cache = HashMap::new();
+    backtrack(&triangle, 0, 0, &mut cache)
 }
