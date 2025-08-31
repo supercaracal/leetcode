69458c1e6784f07e3605ccf7ebaedad02806c88b
Taishi Kasuga <supercaracal@yahoo.com>
2025-08-31 09:14:45 +0900

    Fix 121

--- 0121_best_time_to_bu_and_sell_stock.rs
+++ 0121_best_time_to_bu_and_sell_stock.rs
@@ -11,8 +11,26 @@ fn main() -> Result<(), &'static str> {
     Ok(())
 }
 
+//  3  2  6  5  0  3
+//    -1  4 -1 -5  3
+//
+//  7  1  5  3  6  4
+//    -6  4 -2  3 -2
 fn max_profit(prices: Vec<i32>) -> i32 {
-    // TODO: solve
-    println!("{prices:?}");
-    0
+    // TODO: optimize
+    let mut profit = 0;
+    let mut max_profit = 0;
+    for i in 0..prices.len() {
+        if i == 0 {
+            continue;
+        }
+        let diff = prices[i] - prices[i - 1];
+        if profit < 0 && diff > 0 {
+            profit = diff;
+        } else {
+            profit += diff;
+        }
+        max_profit = max_profit.max(profit);
+    }
+    max_profit
 }
