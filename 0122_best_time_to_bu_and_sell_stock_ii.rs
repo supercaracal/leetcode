5d349f99614a89c3ad8f9695fd05ddb0a72b7324
Taishi Kasuga <supercaracal@yahoo.com>
2025-09-07 09:27:18 +0900

    Fix 122

--- 0122_best_time_to_bu_and_sell_stock_ii.rs
+++ 0122_best_time_to_bu_and_sell_stock_ii.rs
@@ -12,7 +12,26 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn max_profit(prices: Vec<i32>) -> i32 {
-    // TODO: solve
-    println!("{prices:?}");
-    0
+    // TODO: optimize, simplify
+    let mut holding = prices[0];
+    let mut profit = 0;
+    let mut total = 0;
+    for v in prices.iter().skip(1) {
+        println!("holding={holding}, profit={profit}, total={total}, v={v}");
+        if &holding >= v {
+            holding = *v;
+            total += profit;
+            profit = 0;
+        } else if profit < v - holding {
+            profit = v - holding;
+        } else {
+            holding = *v;
+            total += profit;
+            profit = 0;
+        }
+    }
+    if profit > 0 {
+        total += profit;
+    }
+    total
 }
