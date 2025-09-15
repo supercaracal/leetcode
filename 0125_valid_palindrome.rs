26f7a2d2d6e8e8874684fd36524031e0e8e5695a
Taishi Kasuga <supercaracal@yahoo.com>
2025-09-15 09:48:14 +0900

    Fix 125

--- 0125_valid_palindrome.rs
+++ 0125_valid_palindrome.rs
@@ -8,7 +8,31 @@ fn main() -> Result<(), &'static str> {
 }
 
 fn is_palindrome(s: String) -> bool {
-    // TODO: solve
-    println!("{s:?}");
-    false
+    let s = s.chars().collect::<Vec<char>>();
+    let mut l = 0;
+    let mut r = s.len() - 1;
+    while l <= r {
+        while !s[l].is_ascii_alphanumeric() {
+            if l + 1 == s.len() {
+                break;
+            }
+            l += 1;
+        }
+        while !s[r].is_ascii_alphanumeric() {
+            if r == 0 {
+                break;
+            }
+            r -= 1;
+        }
+        println!("{}, {}", s[l], s[r]);
+        if l + 1 == s.len() || r == 0 {
+            break;
+        } else if s[l].eq_ignore_ascii_case(&s[r]) {
+            l += 1;
+            r -= 1;
+        } else {
+            return false;
+        }
+    }
+    true
 }
